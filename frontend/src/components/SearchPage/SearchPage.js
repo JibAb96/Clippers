33013e67f0769ad3cac43b43e1a35b7fe6947bf5
JibAb96 <j.abshir96@gmail.com>
2024-12-24 17:25:31 +0100

    Create a Search Page for users to find clippers.

--- frontend/src/components/SearchPage/SearchPage.js
+++ frontend/src/components/SearchPage/SearchPage.js
@@ -1,12 +1,25 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import ClipperProfiles from "./ClipperProfiles";
+import Filter from "./Filter";
 const SearchPage = () => {
-    return(
-        <div>
-            <SearchBar/>
-            <hr className="mt-5"/>
-        </div>
-    );
+  // Search Page so far includes a title, a searc bar, filter and section with clipper profiles
+    return (
+    <div>
+      <h1 className="font-bold font-DM text-4xl text-center mb-10">
+        Discover
+        <span className="text-skyblue font-DM font-bold tracking-wide">
+          {" "}
+          clippers
+        </span>
+        , Amplify Your Reach
+      </h1>
+      <SearchBar />
+      <hr className="mt-5" />
+      <Filter/>
+      <ClipperProfiles/>
+    </div>
+  );
 };
 
 export default SearchPage;
