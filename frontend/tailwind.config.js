d6003f258f9b7c95871bd4372b5dc9183a738ce5
JibAb96 <j.abshir96@gmail.com>
2024-12-24 07:49:36 +0100

    Add custom color and font to tailwind config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        skyblue: '#24b5eb', 
+      },
+      fontFamily: {
+        DM: ["League Spartan"]
+     }
+    },
   },
   plugins: [],
 }
