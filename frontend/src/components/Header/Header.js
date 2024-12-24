21e93e83095b9ed0e70b4897037b11fda3eca969
JibAb96 <j.abshir96@gmail.com>
2024-12-24 17:23:01 +0100

    Add comments.

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Logo from "./Logo.js";
 import NavigationButton from "./Navigation/NavigationButton.js";
+
+// Header that wil be featured through out the page inclusing Logo, Navigation Button and
+// link to become a clipper 
+
 const Header = () => {
   return (
     <div className="w-100 flex justify-between p-5">
