27497f71290204af2d8d8c0aa2d72cd313d9b11e
JibAb96 <j.abshir96@gmail.com>
2024-12-24 07:54:59 +0100

    Create component with Search Page.

---
+++ frontend/src/components/SearchPage/SearchPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+const SearchPage = () => {
+    return(
+        <div>
+            <SearchBar/>
+            <hr className="mt-5"/>
+        </div>
+    );
+};
+
+export default SearchPage;
