89e88bf701b6db483cf6034a3b3a7598a9bad7c7
JibAb96 <j.abshir96@gmail.com>
2024-12-24 07:51:29 +0100

    Create Logo component for project

---
+++ frontend/src/components/Header/Logo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
+const Logo = () => {
+  return (
+    <div className="flex bg-white w-40 justify-center items-center	">
+      <FontAwesomeIcon
+        icon={faPaperclip}
+        className="size-7 rotate-180 pl-1 text-skyblue"
+      />
+      <a href=".">
+        <h1 className="text-skyblue font-DM text-2xl font-bold tracking-wide">
+          clippers
+        </h1>
+      </a>
+    </div>
+  );
+};
+
+export default Logo;
