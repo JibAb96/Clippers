31c29d3616bd483b9d01a00a0fd54e110edf95a3
JibAb96 <j.abshir96@gmail.com>
2024-12-24 17:26:06 +0100

    Create a Search context to use states and functions globally.

---
+++ frontend/src/context/SearchContext.js
@@ -0,0 +1,43 @@
+import React, { createContext, useState } from "react";
+import clippers from "../db";
+export const SearchContext = createContext();
+
+export const SearchProvider = ({ children }) => {
+  // These states are for the search bar input, list of filters selected and list of clippers
+  // after being filtered by category 
+  const [search, setSearch] = useState("");
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [filteredClippers, setFilteredClippers] = useState(clippers);
+
+  // This is a function that filter the list of clippers based on the categories in the 
+  // selectedFilters state.
+  const filterClippers = () => {
+    if (selectedFilters.length > 0) {
+      let tempClippers = selectedFilters.map((selectedCategory) => {
+        let temp = clippers.filter(
+          (clipper) => clipper.niche === selectedCategory
+        );
+        return temp;
+      });
+      setFilteredClippers(tempClippers.flat());
+    } else {
+      setFilteredClippers([...clippers]);
+    }
+  };
+
+  return (
+    <SearchContext.Provider
+      value={{
+        search,
+        setSearch,
+        filteredClippers,
+        setFilteredClippers,
+        selectedFilters,
+        setSelectedFilters,
+        filterClippers
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+};
