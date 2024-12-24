0a82e18f383784745fe54ded328dd392d5a85fe8
JibAb96 <j.abshir96@gmail.com>
2024-12-24 17:21:56 +0100

    Create an array listing all the clippers information.

---
+++ frontend/src/db.js
@@ -0,0 +1,191 @@
+import ImJustBait from "./images/IMJUSTBAIT-Thumb.jpg"
+import maliworldent from "./images/maliworldent-thumb.jpg"
+import Placeholder from "./images/thumbnail.png"
+
+
+// This is a variable assigned to an array with a list of objects with information 
+// about clippers
+const clippers = [
+    {
+      "id": 1,
+      "name": "ImJustBait",
+      "followers": 5000000,
+      "platform": "Instagram",
+      "niche": "Entertainment",
+      "price": 5000,
+      "thumbnail": ImJustBait
+    },
+    {
+      "id": 2,
+      "name": "maliworldent",
+      "followers": 191000,
+      "platform": "Instagram",
+      "niche": "Entertainment",
+      "price": 200,
+      "thumbnail": maliworldent
+    },
+    {
+        "id": 3,
+        "name": "HoustonPlug",
+        "followers": 850000,
+        "platform": "Instagram",
+        "niche": "Entertainment",
+        "price": 1000,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 4,
+        "name": "BasketballVids",
+        "followers": 2100000,
+        "platform": "TikTok",
+        "niche": "Sport",
+        "price": 2500,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 5,
+        "name": "TechDeals",
+        "followers": 450000,
+        "platform": "Twitter",
+        "niche": "Technology",
+        "price": 800,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 6,
+        "name": "GamingDaily",
+        "followers": 1500000,
+        "platform": "YouTube",
+        "niche": "Gaming",
+        "price": 3000,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 7,
+        "name": "FitnessInspo",
+        "followers": 750000,
+        "platform": "Instagram",
+        "niche": "Fitness",
+        "price": 1200,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 8,
+        "name": "FoodieHeaven",
+        "followers": 980000,
+        "platform": "TikTok",
+        "niche": "Food",
+        "price": 1500,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 9,
+        "name": "StyleSpotter",
+        "followers": 620000,
+        "platform": "Instagram",
+        "niche": "Fashion",
+        "price": 900,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 10,
+        "name": "ComedyClips",
+        "followers": 3200000,
+        "platform": "TikTok",
+        "niche": "Entertainment",
+        "price": 4000,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 11,
+        "name": "PetLovers",
+        "followers": 890000,
+        "platform": "Instagram",
+        "niche": "Pets",
+        "price": 1100,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 12,
+        "name": "CrepCheck",
+        "followers": 1700000,
+        "platform": "Twitter",
+        "niche": "Fashion",
+        "price": 2000,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 13,
+        "name": "StreetCulture",
+        "followers": 550000,
+        "platform": "Instagram",
+        "niche": "Entertainment",
+        "price": 800,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 14,
+        "name": "TravelVibes",
+        "followers": 1200000,
+        "platform": "Instagram",
+        "niche": "Travel",
+        "price": 1800,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 15,
+        "name": "ArtDaily",
+        "followers": 420000,
+        "platform": "Instagram",
+        "niche": "Art",
+        "price": 600,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 16,
+        "name": "BeautyCentral",
+        "followers": 880000,
+        "platform": "YouTube",
+        "niche": "Beauty",
+        "price": 1400,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 17,
+        "name": "SportsHighlight",
+        "followers": 2500000,
+        "platform": "Twitter",
+        "niche": "Sports",
+        "price": 3000,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 18,
+        "name": "MovieBuzz",
+        "followers": 930000,
+        "platform": "Instagram",
+        "niche": "Entertainment",
+        "price": 1300,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 19,
+        "name": "TechReviews",
+        "followers": 670000,
+        "platform": "YouTube",
+        "niche": "Technology",
+        "price": 1000,
+        "thumbnail": Placeholder
+    },
+    {
+        "id": 20,
+        "name": "AutomotiveWorld",
+        "followers": 580000,
+        "platform": "Instagram",
+        "niche": "Automotive",
+        "price": 900,
+        "thumbnail": Placeholder
+    }
+]
+
+export default clippers
