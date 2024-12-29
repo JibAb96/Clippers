e059258128380a853b6b56ba811ee20f70bf2762
JibAb96 <j.abshir96@gmail.com>
2024-12-29 17:06:38 +0100

    Create page for registering users.

---
+++ frontend/src/components/Registration/Register.js
@@ -0,0 +1,170 @@
+import React, { useState } from "react";
+import CTAButton from "../Buttons/CTAButton";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    mobile: "",
+    password: "",
+    cpassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value, 
+    }));
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center font-[sans-serif] py-32 px-6">
+      <div className="grid md:grid-cols-2 items-center gap-y-8 bg-white max-w-7xl w-full shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md overflow-hidden">
+        <div className="max-md:order-1 flex flex-col justify-center sm:p-8 p-4 bg-gradient-to-r from-blue-800 to-skyblue w-full h-full">
+          <div className="max-w-md space-y-12 mx-auto">
+            <div>
+              <h4 className="text-white text-lg font-semibold">
+                Create Your Account
+              </h4>
+              <p className="text-[13px] text-white mt-2">
+                Welcome to our registration page! Get started by creating your
+                account.
+              </p>
+            </div>
+            <div>
+              <h4 className="text-white text-lg font-semibold">
+                Simple & Secure Registration
+              </h4>
+              <p className="text-[13px] text-white mt-2">
+                Our registration process is designed to be straightforward and
+                secure. We prioritize your privacy and data security.
+              </p>
+            </div>
+            <div>
+              <h4 className="text-white text-lg font-semibold">
+                Terms and Conditions Agreement
+              </h4>
+              <p className="text-[13px] text-white mt-2">
+                Require users to accept the terms and conditions of your service
+                during registration.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <form className="sm:p-8 p-4 w-full">
+          <div className="mb-12">
+            <h3 className="text-3xl font-extrabold max-md:text-center">
+              Register
+            </h3>
+          </div>
+
+          <div className="grid lg:grid-cols-2 gap-6">
+            <div>
+              <label className="text-gray-800 text-sm mb-2 block">
+                First Name
+              </label>
+              <input
+                onChange={handleChange}
+                value={formData.fname}
+                name="fname"
+                type="text"
+                className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter name"
+              />
+            </div>
+            <div>
+              <label className="text-gray-800 text-sm mb-2 block">
+                Last Name
+              </label>
+              <input
+                onChange={handleChange}
+                value={formData.lname}
+                name="lname"
+                type="text"
+                className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter last name"
+              />
+            </div>
+            <div>
+              <label className="text-gray-800 text-sm mb-2 block">Email</label>
+              <input
+                onChange={handleChange}
+                value={formData.email}
+                name="email"
+                type="email"
+                className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter email"
+              />
+            </div>
+            <div>
+              <label className="text-gray-800 text-sm mb-2 block">
+                Mobile No.
+              </label>
+              <input
+                onChange={handleChange}
+                value={formData.mobile}
+                name="mobile"
+                type="number"
+                className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter mobile number"
+              />
+            </div>
+            <div>
+              <label className="text-gray-800 text-sm mb-2 block">
+                Password
+              </label>
+              <input
+                onChange={handleChange}
+                value={formData.password}
+                name="password"
+                type="password"
+                className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter password"
+              />
+            </div>
+            <div>
+              <label className="text-gray-800 text-sm mb-2 block">
+                Confirm Password
+              </label>
+              <input
+                onChange={handleChange}
+                value={formData.cpassword}
+                name="cpassword"
+                type="password"
+                className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter confirm password"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center mt-6">
+            <input
+              id="remember-me"
+              name="remember-me"
+              type="checkbox"
+              className="h-4 w-4 shrink-0 rounded"
+            />
+            <label htmlFor="remember-me" className="ml-3 block text-sm">
+              I accept the{" "}
+              <a
+                href="."
+                className="text-gray-800 font-semibold hover:underline ml-1"
+              >
+                Terms and Conditions
+              </a>
+            </label>
+          </div>
+
+          <div className="mt-6">
+            <CTAButton CTAM={"Sign Up"} CustomClass={"px-14 font-semibold"} />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
