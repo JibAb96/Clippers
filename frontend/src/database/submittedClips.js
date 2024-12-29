2679ddb227e33136c88e5d90aea0bfee549617ca
JibAb96 <j.abshir96@gmail.com>
2024-12-29 17:23:49 +0100

    Create file with information for clips submitted by user for creator dashboard.

---
+++ frontend/src/database/submittedClips.js
@@ -0,0 +1,77 @@
+import Atlantis from "../images/Thumbnail-JH.jpg";
+import BookRecommendation from "../images/Thumbnail-TM.jpg";
+import TimeManagement from "../images/Thumnail-1.webp";
+import StayFit from "../images/Thumbnail-fitness.jpg";
+import BetterSleep from "../images/Thumbnail-sleep.jpg";
+import CodingHacks from "../images/Thumnail-coding.jpg";
+import VeganRecipes from "../images/Thumnail-vegan.png";
+import AncientEgypt from "../images/thumbnail-egypt.jpeg";
+//Information for clips submitted by user
+const submittedClips = [
+  {
+    id: 1,
+    name: "Is Atlantis Real?",
+    status: "Posted",
+    clipper: "ImJustBait",
+    platform: "Instagram",
+    thumbnail: Atlantis,
+  },
+  {
+    id: 2,
+    name: "Book Recommendations",
+    status: "Pending",
+    clipper: "Naseeha",
+    platform: "TikTok",
+    thumbnail: BookRecommendation,
+  },
+  {
+    id: 3,
+    name: "Manage Your Time",
+    status: "Rejected",
+    clipper: "ProductivityGuru",
+    platform: "YouTube",
+    thumbnail: TimeManagement,
+  },
+  {
+    id: 4,
+    name: "How to Stay Fit",
+    status: "Posted",
+    clipper: "FitnessPro",
+    platform: "YouTube",
+    thumbnail: StayFit,
+  },
+  {
+    id: 5,
+    name: "10 Tips for Better Sleep",
+    status: "Pending",
+    clipper: "SleepWell",
+    platform: "Instagram",
+    thumbnail: BetterSleep,
+  },
+  {
+    id: 6,
+    name: "Top 5 Coding Hacks",
+    status: "Rejected",
+    clipper: "CodeMaster",
+    platform: "TikTok",
+    thumbnail: CodingHacks,
+  },
+  {
+    id: 7,
+    name: "Easy Vegan Recipes",
+    status: "Posted",
+    clipper: "HealthyEats",
+    platform: "YouTube",
+    thumbnail: VeganRecipes,
+  },
+  {
+    id: 8,
+    name: "Exploring Ancient Egypt",
+    status: "Pending",
+    clipper: "HistoryBuff",
+    platform: "Instagram",
+    thumbnail: AncientEgypt,
+  },
+];
+
+export default submittedClips
