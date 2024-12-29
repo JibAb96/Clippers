2ce63418e58451f6fe83b441d3ada7972df39a6a
JibAb96 <j.abshir96@gmail.com>
2024-12-29 16:55:21 +0100

    Add CTA button to header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,17 +1,15 @@
 import React from "react";
 import Logo from "./Logo.js";
 import NavigationButton from "./Navigation/NavigationButton.js";
+import CTAButton from "../Buttons/CTAButton.js";
 
 // Header that wil be featured through out the page inclusing Logo, Navigation Button and
 // link to become a clipper 
-
 const Header = () => {
   return (
-    <div className="w-100 flex justify-between p-5">
+    <div className="w-full absolute top-0 left-0 z-50 py-4 flex justify-between bg-white bg-opacity-10">
       <Logo />
-      <h1 className="mt-4 ml-[40rem] font-semibold hover:text-skyblue cursor-pointer">
-        Become a Clipper
-      </h1>
+      <CTAButton CustomClass="hidden absolute right-32 top-5" CTAM="Become a Clipper"/>
       <NavigationButton className="flex-end" />
     </div>
   );
