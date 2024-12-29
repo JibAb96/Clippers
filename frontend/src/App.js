d620ec1ed0a5da0dde322f7c111df5dbe38e7bff
JibAb96 <j.abshir96@gmail.com>
2024-12-29 16:44:27 +0100

    Import React Browser Routing, Add components for Dashboard, ClipperProfile, Register, SignIn and React Suspense. Make paths to render components on.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,31 @@
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header.js";
 import SearchPage from "./components/SearchPage/SearchPage.js";
 import { SearchProvider } from "./context/SearchContext.js";
+import ClipperProfile from "./components/ClipperProfile/ClipperProfile.js";
+import CreatorDashboard from "./components/CreatorDashboard/CreatorDashboard.js";
+import Loader from "./components/Utilities/Loader.js";
+import Register from "./components/Registration/Register.js";
+import SignIn from "./components/SignIn/SignIn.js";
+
 function App() {
-  // Imported components to be displayed, only working on search page so no routes
   return (
-    <div>
+    <Router>
       <SearchProvider>
         <Header />
-        <SearchPage />
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+            <Route path="/profile/:id" element={<ClipperProfile />} />
+            <Route path="/dashboard" element={<CreatorDashboard />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </Suspense>
       </SearchProvider>
-    </div>
+    </Router>
   );
 }
 
