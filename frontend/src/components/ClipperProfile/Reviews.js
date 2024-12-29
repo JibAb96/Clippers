ebb1d570ed31e16445ab2439ab6fd6ace9b135bf
JibAb96 <j.abshir96@gmail.com>
2024-12-29 17:10:47 +0100

    Create a section on clipper profiles displaying reviews from content creators.

---
+++ frontend/src/components/ClipperProfile/Reviews.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+const Reviews = ({ Clipper }) => {
+  //Section of Clipper profiles displaying reviews from other creators.
+  return (
+    <div className=" ml-5 my-3 xl:ml-20 xl:my-10">
+      <h1 className="text-skyblue font-semibold text-xl sm:text-2xl  xl:text-3xl">Creator Reviews</h1>
+      <div>
+        {Clipper.reviews.map((review) => (
+          <div className="mt-5 xl:mt-10">
+            <div className="flex gap-5">
+              <h2 className="font-semibold">{review.creator}</h2>
+              <div>
+                {Array.from({ length: Number(review.stars) }).map(
+                  (_, index) => (
+                    <FontAwesomeIcon icon={faStar} key={index} className="text-yellow-400" />
+                  )
+                )}
+              </div>
+            </div>
+            <p className="my-3 lg:w-3/4 xl:my-5 font-light">{review.review}</p>
+           <hr/> 
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
