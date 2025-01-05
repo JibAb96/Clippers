a0c201c10482e64db470787e4ca7418a4751e7ce
JibAb96 <j.abshir96@gmail.com>
2025-01-05 11:27:39 +0100

    Create type for objects containing a sent or submitted clip. Also apply prettier format.

--- frontend/src/model.tsx
+++ frontend/src/model.tsx
@@ -1,26 +1,45 @@
-import { IconDefinition } from "@fortawesome/fontawesome-svg-core"
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
 export type Review = {
-    creator: string
-    stars: number
-    review: string
-}
+  creator: string;
+  stars: number;
+  review: string;
+};
 
 export type Clipper = {
-  id: number
-  name: string
-  followers: number
-  platform: "Instagram" | "Twitter" | "TikTok" | "YouTube",
-  niche: string
-  price: number
-  thumbnail: string
-  images: string[]
-  guidelines: string[]
-  reviews: Review[]
-}
+  id: number;
+  name: string;
+  followers: number;
+  platform: "Instagram" | "Twitter" | "TikTok" | "YouTube";
+  niche: string;
+  price: number;
+  thumbnail: string;
+  images: string[];
+  guidelines: string[];
+  reviews: Review[];
+};
 
 export type Category = {
-  id: number
-  icon: IconDefinition
-  name: string
-}
+  id: number;
+  icon: IconDefinition;
+  name: string;
+};
+
+export type SentClip = {
+  status: "Rejected" | "Pending" | "Posted";
+};
+
+export type SubmittedClip = {
+  status: "New Submission" | "Rejected" | "Ready For Upload" | "Posted";
+};
+
+type CombinedStatus = SentClip["status"] | SubmittedClip["status"];
+
+export type Clip = {
+  id: number;
+  name: string;
+  status: CombinedStatus;
+  user: string;
+  platform: string;
+  thumbnail: string;
+};
