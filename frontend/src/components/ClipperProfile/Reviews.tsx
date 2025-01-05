33ccb72099a8ad862c1eb598b5c45cb975bec094
JibAb96 <j.abshir96@gmail.com>
2025-01-05 11:36:18 +0100

    Add keys to elements rendered with map method.

--- frontend/src/components/ClipperProfile/Reviews.tsx
+++ frontend/src/components/ClipperProfile/Reviews.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { Clipper } from "../../model";
-
 type Props = {
   Clipper: Clipper
 }
@@ -13,8 +12,8 @@ const Reviews = ({ Clipper }: Props) => {
     <div className=" ml-5 my-3 xl:ml-20 xl:my-10">
       <h1 className="text-skyblue font-semibold text-xl sm:text-2xl  xl:text-3xl">Creator Reviews</h1>
       <div>
-        {Clipper.reviews.map((review) => (
-          <div className="mt-5 xl:mt-10">
+        {Clipper.reviews.map((review, index) => (
+          <div key={index} className="mt-5 xl:mt-10">
             <div className="flex gap-5">
               <h2 className="font-semibold">{review.creator}</h2>
               <div>
