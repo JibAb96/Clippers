0d22e38e103c9041491daa567447c406dd40c464
JibAb96 <j.abshir96@gmail.com>
2025-01-05 11:29:46 +0100

    Create ClipperDashboard displaying clips submitted to a clipper, categorising and filtering clips by status.

---
+++ frontend/src/components/ClipperDashboard/ClipperDashboard.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import ClipStatusHeadings from "../Utilities/ClipStatusHeadings";
+import DashboardSections from "../Utilities/DashboardSections";
+import SubmittedClips from "../Utilities/SubmittedClips";
+import { useSearchContext } from "../../context/SearchContext";
+import submittedClips from "../../database/submittedClips";
+
+const ClipperDashboard = () => {
+  const { filteredClips, filterClips, status } = useSearchContext();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    filterClips(submittedClips);
+    // eslint-disable-next-line
+  }, [status]);
+
+  return (
+    <div>
+      <div className="bg-gradient-to-r from-blue-300 to-blue-500  pt-20">
+        <h1 className="text-3xl my-10 font-semibold text-white text-center sm:text-left sm:ml-10">
+          Clipper Dashboard
+        </h1>
+        <ClipStatusHeadings
+          Clips={submittedClips}
+          HeadingOne="Ready For Upload"
+          HeadingTwo="Posted"
+          HeadingThree="Rejected"
+          HeadingFour="New Submission"
+        />
+        <DashboardSections
+          HeadingOne="New Submission"
+          HeadingTwo="Ready For Upload"
+          HeadingThree="Posted"
+          HeadingFour="Rejected"
+        />
+        <SubmittedClips filteredClips={filteredClips} />
+      </div>
+    </div>
+  );
+};
+
+export default ClipperDashboard;
