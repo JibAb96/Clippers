98865d78c411d49f69bbdaf82f71ede973bb14c0
JibAb96 <j.abshir96@gmail.com>
2025-01-05 11:45:27 +0100

    Add on click property to CTAButton, so that it navigates the use to clipper dashboard.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import Logo from "./Logo";
 import NavigationButton from "./Navigation/NavigationButton";
 import CTAButton from "../Buttons/CTAButton";
+import { useNavigate } from "react-router-dom";
 
 // Header that wil be featured through out the page inclusing Logo, Navigation Button and
-// link to become a clipper 
+// link to become a clipper
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className="w-full absolute top-0 left-0 z-50 py-4 flex justify-between bg-white bg-opacity-10">
       <Logo />
-      <CTAButton CustomClass="hidden absolute right-32 top-5" CTAM="Become a Clipper"/>
-      <NavigationButton/>
+      <CTAButton
+        CustomClass="hidden absolute right-32 top-5"
+        CTAM="Become a Clipper"
+        onClick={() => navigate("/clipper")}
+      />
+      <NavigationButton />
     </div>
   );
 };
