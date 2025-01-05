7baf68ccb60dbddad9f12729fbbb08b9cae09728
JibAb96 <j.abshir96@gmail.com>
2025-01-05 12:00:55 +0100

    Add states and functions to SearchContext made for filtering clips on dashboards.

--- frontend/src/context/SearchContext.tsx
+++ frontend/src/context/SearchContext.tsx
@@ -1,27 +1,38 @@
-import React, { createContext,useContext, useState, PropsWithChildren } from "react";
-import { Clipper, Category } from "../model"; 
+import React, {
+  createContext,
+  useContext,
+  useState,
+  PropsWithChildren,
+} from "react";
+import { Clipper, Category, Clip } from "../model";
 import clippers from "../database/clippers";
 type SearchContextType = {
-  search: string
-  setSearch: React.Dispatch<React.SetStateAction<string>>
-  filteredClippers: Clipper[]
-  setFilteredClippers: React.Dispatch<React.SetStateAction<Clipper[]>>
-  selectedFilters: Category[]
-  setSelectedFilters: React.Dispatch<React.SetStateAction<Category[]>>
-  filterClippers: () => void
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+  filteredClippers: Clipper[];
+  setFilteredClippers: React.Dispatch<React.SetStateAction<Clipper[]>>;
+  selectedFilters: Category[];
+  setSelectedFilters: React.Dispatch<React.SetStateAction<Category[]>>;
+  filterClippers: () => void;
+  filterClips: (clips: Clip[]) => void;
+  filteredClips: Clip[];
+  setFilteredClips: React.Dispatch<React.SetStateAction<Clip[]>>;
+  setStatus: React.Dispatch<React.SetStateAction<string>>;
+  status: string;
 };
 export const SearchContext = createContext<SearchContextType>(null!);
 
-export const useSearchContext = () => useContext(SearchContext)
+export const useSearchContext = () => useContext(SearchContext);
 
 export const SearchProvider = ({ children }: PropsWithChildren) => {
   // These states are for the search bar input, list of filters selected and list of clippers
   // after being filtered by category
 
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState<string>("");
   const [selectedFilters, setSelectedFilters] = useState<Category[]>([]);
-  const [filteredClippers, setFilteredClippers] = useState(clippers);
-
+  const [filteredClippers, setFilteredClippers] = useState<Clipper[]>(clippers);
+  const [status, setStatus] = useState<string>("");
+  const [filteredClips, setFilteredClips] = useState<Clip[]>([]);
   // This is a function that filter the list of clippers based on the categories in the
   // selectedFilters state.
   const filterClippers = () => {
@@ -38,16 +49,32 @@ export const SearchProvider = ({ children }: PropsWithChildren) => {
     }
   };
 
+  const filterClips = (clips: Clip[]) => {
+    if (clips) {
+      if (status) {
+        let tempClips = clips.filter((clip: Clip) => clip.status === status);
+        setFilteredClips(tempClips.flat());
+      } else {
+        setFilteredClips(clips);
+      }
+    }
+  };
+
   return (
     <SearchContext.Provider
       value={{
         search,
         setSearch,
         filterClippers,
+        filterClips,
         filteredClippers,
+        filteredClips,
+        setFilteredClips,
         setFilteredClippers,
         selectedFilters,
         setSelectedFilters,
+        setStatus,
+        status,
       }}
     >
       {children}
