59cce7b2296fcd3bc4b6b25af15e7a2424ae20a6
JibAb96 <j.abshir96@gmail.com>
2025-01-05 11:44:17 +0100

    Add filter functionality to dashboard using data and function from SearchContext,

--- frontend/src/components/CreatorDashboard/CreatorDashboard.tsx
+++ frontend/src/components/CreatorDashboard/CreatorDashboard.tsx
@@ -1,21 +1,38 @@
 import React, { useEffect } from "react";
-import ClipStatusHeadings from "./ClipStatusHeadings";
-import SubmittedClips from "./SubmittedClips";
-import DashboardSections from "./DashboardSections";
-
+import ClipStatusHeadings from "../Utilities/ClipStatusHeadings";
+import SubmittedClips from "../Utilities/SubmittedClips";
+import DashboardSections from "../Utilities/DashboardSections";
+import sentClips from "../../database/sentClips";
+import { useSearchContext } from "../../context/SearchContext";
 
 const CreatorDashboard = () => {
+  const { filteredClips, filterClips, status } = useSearchContext();
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    filterClips(sentClips);
+    // eslint-disable-next-line
+  }, [status]);
+
   return (
     <div className="bg-gradient-to-r from-blue-800 to-skyblue pt-20">
       <h1 className="text-3xl my-10 font-semibold text-white text-center sm:text-left sm:ml-10">
         Creator Dashboard
       </h1>
-      <ClipStatusHeadings/>
-      <DashboardSections/>
-      <SubmittedClips/>
+      <ClipStatusHeadings
+        Clips={sentClips}
+        HeadingOne="Pending"
+        HeadingTwo="Posted"
+        HeadingThree="Rejected"
+      />
+      <DashboardSections
+        HeadingOne="Pending"
+        HeadingTwo="Posted"
+        HeadingThree="Rejected"
+      />
+      <SubmittedClips filteredClips={filteredClips} />
     </div>
   );
 };
