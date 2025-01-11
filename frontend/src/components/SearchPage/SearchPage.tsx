29ad6ff9c91600020cfd9e4a7e8290561c91cc82
JibAb96 <j.abshir96@gmail.com>
2025-01-11 09:58:57 +0100

    Move SearchBar and h1 element to the Header. Incoporate dispatch from useReducer for actions regarding the filteredClippers state. Add comments.

--- frontend/src/components/SearchPage/SearchPage.tsx
+++ frontend/src/components/SearchPage/SearchPage.tsx
@@ -1,37 +1,34 @@
 import React, { useEffect } from "react";
-import SearchBar from "./SearchBar";
-import ClippersDisplay from "./ClippersDisplay";
-import Filter from "./Filter";
-import { useSearchContext } from "../../context/SearchContext";
+import ClippersDisplay from "./ClippersDisplay"; 
+import Filter from "./Filter"; 
+import { useSearchContext } from "../../context/SearchContext"; 
+import { ACTIONS } from "../../model";
 
 const SearchPage = () => {
-  // Search Page so far includes a title, a searc bar, filter and section with clipper profiles
-  const { filterClippers, selectedFilters } = useSearchContext();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  // Destructuring context values for dispatch function and selectedFilters from the SearchContext
+  const { dispatch, selectedFilters } = useSearchContext();
 
+  // Effect hook to ensure the page scrolls to the top when the component mounts
   useEffect(() => {
-    filterClippers();
+    window.scrollTo(0, 0); // Scroll to top
+  }, []); // Empty dependency array ensures this effect runs only once when component mounts
+
+  // Effect hook to dispatch an action to filter clippers when selectedFilters change
+  useEffect(() => {
+    dispatch({
+      type: ACTIONS.FILTER_CLIPPERS, // Dispatch the action to filter the clippers
+      payload: { selectedFilters: selectedFilters }, // Pass the selected filters as payload
+    });
     // eslint-disable-next-line
-  }, [selectedFilters]);
+  }, [selectedFilters]); // Dependency array ensures this effect runs when selectedFilters change
 
   return (
-    <div className="pt-20">
-      <h1 className="font-bold font-DM text-4xl text-center my-10">
-        Discover
-        <span className="text-skyblue font-DM font-bold tracking-wide">
-          {" "}
-          clippers
-        </span>
-        , Amplify Your Reach
-      </h1>
-      <SearchBar />
-      <hr className="mt-5" />
-      <Filter />
-      <ClippersDisplay />
+    <div className="pt-10 lg:pt-56 bg-[#FAFAFA]">
+      <hr className="md:mt-5" /> {/* Horizontal line to separate sections */}
+      <Filter /> {/* Render the Filter component for the user to select filters */}
+      <ClippersDisplay /> {/* Render the ClippersDisplay component to show filtered clippers */}
     </div>
   );
 };
 
-export default SearchPage;
+export default SearchPage; 
