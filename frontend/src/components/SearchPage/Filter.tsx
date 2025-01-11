97304354f8fab0646d4c0a7997eb13ff74c13ae4
JibAb96 <j.abshir96@gmail.com>
2025-01-11 10:14:04 +0100

    Add categories for social media platforms that users can filter by. Incorporate dispatch for actions to the filteredClippers state. Disable other category buttons if one is selected. Add button to filter clippers based on price of service. Adjust styling accordingly. Add comments.

--- frontend/src/components/SearchPage/Filter.tsx
+++ frontend/src/components/SearchPage/Filter.tsx
@@ -10,23 +10,40 @@ import {
   faBaseballBatBall,
   faShirt,
   faSpa,
+  faArrowUp,
+  faArrowDown,
 } from "@fortawesome/free-solid-svg-icons";
-import { Category } from "../../model";
+import { ACTIONS, Category } from "../../model";
 import { useSearchContext } from "../../context/SearchContext";
+import {
+  faInstagram,
+  faTiktok,
+  faXTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
+// Main Filter component
 const Filter = () => {
-  const { selectedFilters, setSelectedFilters } = useSearchContext();
+  // Context hook to get selected filters and dispatch function for state updates
+  const { selectedFilters, setSelectedFilters, dispatch } = useSearchContext();
+
+  // Refs and state hooks for managing scroll position and button states
   const containerRef = useRef<HTMLDivElement>(null);
   const [scrollPosition, setScrollPosition] = useState(0);
-  //These functions allow for the user to scroll through categories when the screen is smaller
-  //Based on the position of the scroll it also disables the arrow buttons
+  const [isAscending, setIsAscending] = useState(false);
+  const [disableButtons, setDisableButtons] = useState(false);
+  const [disabledButtons, setDisabledButtons] = useState<number[]>([]);
+
+  // Function to handle scrolling to the left (for smaller screens)
   const scrollLeft = () => {
     if (containerRef.current) {
+      // Calculates the new scroll position, ensuring it doesn't go below 0
       const newPosition = Math.max(
         scrollPosition - containerRef.current.offsetWidth / 2,
         0
       );
       setScrollPosition(newPosition);
+      // Smooth scrolling to the new position
       containerRef.current.scrollTo({
         left: newPosition,
         behavior: "smooth",
@@ -34,8 +51,10 @@ const Filter = () => {
     }
   };
 
+  // Function to handle scrolling to the right
   const scrollRight = () => {
     if (containerRef.current) {
+      // Calculates the maximum scroll position
       const maxScrollPosition =
         containerRef.current.scrollWidth - containerRef.current.offsetWidth;
       const newPosition = Math.min(
@@ -43,6 +62,7 @@ const Filter = () => {
         maxScrollPosition
       );
       setScrollPosition(newPosition);
+      // Smooth scrolling to the new position
       containerRef.current.scrollTo({
         left: newPosition,
         behavior: "smooth",
@@ -50,66 +70,116 @@ const Filter = () => {
     }
   };
 
+  // Categories to be displayed, each with an associated icon and name
   const categories: Category[] = [
-    { id: 1, icon: faPlaneDeparture, name: "Travel" },
-    { id: 2, icon: faMasksTheater, name: "Entertainment" },
-    { id: 3, icon: faUtensils, name: "Food" },
-    { id: 4, icon: faDumbbell, name: "Fitness" },
-    { id: 5, icon: faLaptop, name: "Technology" },
-    { id: 6, icon: faSpa, name: "Beauty" },
-    { id: 7, icon: faGamepad, name: "Gaming" },
-    { id: 8, icon: faBaseballBatBall, name: "Sport" },
-    { id: 9, icon: faShirt, name: "Fashion" },
+    { id: 1, icon: faXTwitter, name: "X" },
+    { id: 2, icon: faInstagram, name: "Instagram" },
+    { id: 3, icon: faTiktok, name: "TikTok" },
+    { id: 4, icon: faYoutube, name: "YouTube" },
+    { id: 5, icon: faPlaneDeparture, name: "Travel" },
+    { id: 6, icon: faMasksTheater, name: "Entertainment" },
+    { id: 7, icon: faUtensils, name: "Food" },
+    { id: 8, icon: faDumbbell, name: "Fitness" },
+    { id: 9, icon: faLaptop, name: "Technology" },
+    { id: 10, icon: faSpa, name: "Beauty" },
+    { id: 11, icon: faGamepad, name: "Gaming" },
+    { id: 12, icon: faBaseballBatBall, name: "Sport" },
+    { id: 13, icon: faShirt, name: "Fashion" },
   ];
-  const handleFilterButtonClick = (selectedCategory: Category) => {
-    const isFiltered = selectedFilters.some(
-      (category) => category.id === selectedCategory.id
-    );
+
+  // Function to handle category button click (toggle filter state)
+  const handleCategoryButtonClick = (selectedCategory: Category) => {
+    const isFiltered = selectedFilters.length > 0;
     if (isFiltered) {
-      const filters = selectedFilters.filter(
-        (el) => el.id !== selectedCategory.id
-      );
-      setSelectedFilters(filters);
+      // If filters are already applied, clear them and enable all buttons
+      setSelectedFilters([]);
+      setDisableButtons(false);
     } else {
-      setSelectedFilters([...selectedFilters, selectedCategory]);
+      // If no filters are applied, disable all buttons except the clicked category
+      setDisabledButtons([
+        ...categories
+          .map((category) => category.id)
+          .filter((id) => id !== selectedCategory.id),
+      ]);
+      setDisableButtons(true);
+      console.log(disabledButtons);
+      // Set the clicked category as the selected filter
+      setSelectedFilters([selectedCategory]);
     }
-    return;
+  };
+
+  // Function to handle the sort button click (toggle sorting order)
+  const handleSortButtonClick = () => {
+    setIsAscending(!isAscending);
+    dispatch({
+      type: ACTIONS.SORT_CLIPPERS,
+      payload: { isAscending: isAscending },
+    });
   };
 
   return (
     <div className="flex items-center w-full relative">
+      {/* Sort button */}
+      <div className="ml-5  mb-2">
+        <button
+          className="h-8 w-8 bg-white text-center rounded-full border-2 border-gray-500 sm:w-12 sm:h-12"
+          onClick={handleSortButtonClick}
+        >
+          {!isAscending ? (
+            <FontAwesomeIcon
+              icon={faArrowUp}
+              className="text-lg text-gray-500 sm:text-xl md:text-2xl lg:text-2xl"
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faArrowDown}
+              className="text-lg text-gray-500 sm:text-xl md:text-2xl lg:text-2xl"
+            />
+          )}
+        </button>
+      </div>
+
+      {/* Left scroll button */}
       <button
-        className="absolute left-0 z-10 bg-white bg-opacity-90 hover:text-skyblue text-black font-bold py-2 px-2 disabled:opacity-0"
+        className="z-10 bg-white bg-opacity-90 hover:text-skyblue text-black font-bold py-2 px-2 disabled:opacity-0"
         onClick={scrollLeft}
         disabled={scrollPosition === 0}
       >
         &lt;
       </button>
+
+      {/* Categories container with horizontal scrolling */}
       <div
-        className="flex xl:justify-center overflow-x-auto scrollbar-hide w-full px-8 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
+        className="flex xl:justify-start overflow-x-auto scrollbar-hide w-full px-2 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
         ref={containerRef}
       >
-        <div className="flex gap-4 md:gap-6 lg:gap-8 my-4">
-          {categories.map((category) => (
+        {/* Render category buttons */}
+        <div className="flex gap-6 md:gap-6 lg:gap-6 my-4">
+          {categories.map((category: Category) => (
             <div key={category.id} className="flex-shrink-0">
               <button
-                onClick={() => handleFilterButtonClick(category)}
+                onClick={() => handleCategoryButtonClick(category)}
                 className={`
-                  w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24
+                   sm:w-20 sm:h-20 md:w-24 md:h-24
                   flex flex-col justify-center items-center
-                  rounded-lg bg-white shadow-sm
-                  hover:bg-gray-50 hover:text-skyblue
-                  transition-all duration-200
+                  rounded-lg transition-all duration-200 
                   ${
-                    selectedFilters.some((filter) => filter.id === category.id)
-                      ? "text-skyblue ring-2 ring-skyblue"
-                      : "text-gray-500"
-                  }
+                    selectedFilters.some(
+                      (filter) => filter.id === category.id
+                    ) && "text-skyblue sm:border sm:border-skyblue"
+                  } ${
+                  disableButtons && disabledButtons.includes(category.id)
+                    ? "text-[#FAFAFA] sm:cursor-not-allowed"
+                    : "md:hover:text-skyblue"
+                }
                 `}
+                disabled={
+                  disableButtons && disabledButtons.includes(category.id)
+                }
               >
                 <FontAwesomeIcon
                   icon={category.icon}
-                  className="text-lg sm:text-xl md:text-2xl lg:text-3xl mb-1"
+                  className="text-lg sm:text-xl md:text-2xl lg:text-2xl mb-1"
                 />
                 <p className="text-[10px] sm:text-xs font-medium">
                   {category.name}
@@ -119,6 +189,8 @@ const Filter = () => {
           ))}
         </div>
       </div>
+
+      {/* Right scroll button */}
       <button
         className="absolute right-0 z-10 bg-white bg-opacity-90 hover:text-skyblue text-black font-bold py-2 px-2 disabled:opacity-0"
         onClick={scrollRight}
