9d4a4695037a08da3921dcce323ababcf9733d92
JibAb96 <j.abshir96@gmail.com>
2025-01-19 15:07:46 +0100

    Change navigation options based on the role of the user on the platform.

--- frontend/src/components/Navigation/NavigationButton.tsx
+++ frontend/src/components/Navigation/NavigationButton.tsx
@@ -8,7 +8,7 @@ import { useSearchContext } from "../../context/SearchContext";
 const NavigationButton = () => {
   // Toggle dropdown visibility
 
-  const { isSignedIn } = useSearchContext();
+  const { isSignedIn, user } = useSearchContext();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -23,11 +23,12 @@ const NavigationButton = () => {
         onClick={toggleDropdown}
         type="button"
         className="flex items-center gap-2 border border-gray-200 rounded-full px-2 py-1.5 hover:shadow-md transition-shadow w-24 h-14 justify-center"
+        aria-label="navigation button"
       >
-        <HiMenu className="h-4 w-4 text-gray-800" />
+        <HiMenu className="h-4 w-4 text-gray-800" aria-hidden="false"/>
         <div className="h-4 w-[1px] bg-gray-200"></div>
         <div className="flex items-center justify-center h-6 w-6 bg-gray-500 rounded-full text-white">
-          <HiUser className="h-4 w-4" />
+          <HiUser className="h-4 w-4" aria-hidden="false"/>
         </div>
       </button>
       {isDropdownOpen && (
@@ -67,22 +68,34 @@ const NavigationButton = () => {
             </li>
             {isSignedIn ? (
               <ul>
+                {user?.role === "creator" ? (
+                  <li>
+                    <Link
+                      onClick={closeDropdown}
+                      to="/dashboard"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Creator Dashboard
+                    </Link>
+                  </li>
+                ) : (
+                  <li>
+                    <Link
+                      onClick={closeDropdown}
+                      to="/clipper"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Clipper Dashboard
+                    </Link>
+                  </li>
+                )}
                 <li>
                   <Link
                     onClick={closeDropdown}
-                    to="/dashboard"
+                    to="/profile"
                     className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                   >
-                    Creator Dashboard
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    onClick={closeDropdown}
-                    to="/clipper"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Clipper Dashboard
+                    Profile
                   </Link>
                 </li>
                 <li>
