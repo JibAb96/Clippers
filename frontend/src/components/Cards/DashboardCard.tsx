98e4e6881ed59e9255bde320c2c022dcb9b09748
JibAb96 <j.abshir96@gmail.com>
2025-01-19 14:39:02 +0100

    For cleaners code get rid of props that are for keys in the clips object and just put in the whole clip. Make an onClick function that opens a Modal and sets the clip information to be presented in the modal. Rearrange information on the card so that a date is not presented since that is not part of the user flow anymore.

--- frontend/src/components/Cards/DashboardCard.tsx
+++ frontend/src/components/Cards/DashboardCard.tsx
@@ -1,57 +1,67 @@
 import React from "react";
+import { Clip } from "../../model";
+import { useSearchContext } from "../../context/SearchContext";
 // Here we have the structure of the cards of each clip that will be displayed. Styled with
 // tailwindcss.
 
 type Props = {
-  Image: string;
-  ClipperName: string;
-  Status: string;
-  Platform: string;
-  ClipperTitle: string;
+  clip: Clip
+  openModal?: React.Dispatch<React.SetStateAction<boolean>> //This will be set to true if card is clicked and will display a modal with information concerning the card
 };
 
 const DashboardCard = ({
-  Image,
-  ClipperName,
-  Status,
-  Platform,
-  ClipperTitle,
+  clip,
+  openModal,
 }: Props) => {
+
+  const { setSelectedClip } = useSearchContext();
   // Function to set bg color corresponding with status
   const getStatusColor = (status: string) => {
     switch (status.toLowerCase()) {
       case "posted":
-        return "bg-green-400";
+        return "text-green-700";
       case "rejected":
-        return "bg-red-600";
-      case "pending":
+        return "text-red-600";
+      case "scheduled":
       case "ready for upload":
-        return "bg-yellow-400";
+        return "text-[#B44800]";
+      case "pending review":
       case "new submission":
-        return "bg-skyblue";
+        return "text-[#187AA0]";
       default:
-        return "bg-gray-400";
+        return "text-gray-400";
     }
   };
-  const statusStyle = getStatusColor(Status);
+  const statusStyle = getStatusColor(clip.status);
+
+  const onClick = () => {
+    openModal && setSelectedClip(clip);
+    openModal && openModal(true);
+  }
+
   return (
-    <div className="max-w-72 rounded cursor-pointer sm:max-w-[17.5rem]">
+    <div className="max-w-72 rounded cursor-pointer sm:max-w-[17.5rem]"
+    onClick={onClick}>
       <div className="relative">
         <img
           className="h-56 object-cover rounded-2xl"
-          src={Image}
-          alt={ClipperName}
+          src={clip.thumbnail}
+          alt={ "This is a a thumbnail for the clip " + clip.title}
         />
       </div>
       <div className="px-1 py-4">
         <div
-          className={`text-xl ${statusStyle} rounded-full px-3 py-1 text-white inline-block`}
+          className={`text-xl ${statusStyle} font-bold py-1 inline-block`}
         >
-          {Status}
+          {clip.status}
         </div>
-        <div className="font-semibold text-xl">{ClipperTitle}</div>
-        <div className="font-medium text-l">{ClipperName}</div>
-        <div className="font-medium text-l">{Platform}</div>
+        <div>
+          <div className="font-semibold text-xl">{clip.title}</div>
+          <div className="font-medium text-l text-tertiary">
+            {clip.platform}
+          </div>
+          <div className="font-medium text-l">{clip.user}</div>
+        </div>
       </div>
     </div>
   );
