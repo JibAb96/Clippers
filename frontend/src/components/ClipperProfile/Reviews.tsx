4ef876934f83e793ac7631399618957a9323b661
JibAb96 <j.abshir96@gmail.com>
2025-01-19 14:48:22 +0100

    Mahe heading heirachy on Clipper Profile page correct. Make FontAwesomeIcon visible to page readers.

--- frontend/src/components/ClipperProfile/Reviews.tsx
+++ frontend/src/components/ClipperProfile/Reviews.tsx
@@ -10,16 +10,16 @@ const Reviews = ({ Clipper }: Props) => {
   //Section of Clipper profiles displaying reviews from other creators.
   return (
     <div className=" ml-5 my-3 xl:ml-20 xl:my-10">
-      <h1 className="text-secondary font-semibold text-xl sm:text-2xl  xl:text-3xl">Creator Reviews</h1>
+      <h2 className="text-secondary font-semibold text-xl sm:text-2xl  xl:text-3xl">Creator Reviews</h2>
       <div>
         {Clipper.reviews.map((review, index) => (
           <div key={index} className="mt-5 xl:mt-10">
             <div className="flex gap-5">
-              <h2 className="font-semibold">{review.creator}</h2>
+              <h3 className="font-semibold">{review.creator}</h3>
               <div>
                 {Array.from({ length: Number(review.stars) }).map(
                   (_, index) => (
-                    <FontAwesomeIcon icon={faStar} key={index} className="text-yellow-400" />
+                    <FontAwesomeIcon icon={faStar} key={index} aria-hidden="false" aria-label="star rating" className="text-yellow-400" />
                   )
                 )}
               </div>
