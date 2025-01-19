34fba852fbd1ca84434a997ff9c80ad3d8df73b8
JibAb96 <j.abshir96@gmail.com>
2025-01-19 15:28:46 +0100

    Accomodate for props changes in DashboardCard component and add setIsClipModalOpen to change IsClipModalOpen state.

--- frontend/src/components/Utilities/DisplayClips.tsx
+++ frontend/src/components/Utilities/DisplayClips.tsx
@@ -4,9 +4,10 @@ import { useSearchContext } from "../../context/SearchContext";
 import { Clip } from "../../model";
 type Props = {
   filteredClips: Clip[];
+  setIsClipModalOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const SubmittedClips = ({ filteredClips }: Props) => {
+const DisplayClips = ({ filteredClips, setIsClipModalOpen }: Props) => {
   const { status } = useSearchContext();
 
   useEffect(() => {
@@ -15,19 +16,16 @@ const SubmittedClips = ({ filteredClips }: Props) => {
   //Section of the dashboard displaying all submitted clips and their status
 
   return (
-    <div className="flex flex-wrap justify-center gap-5 text-white sm:justify-start sm:mx-10">
+    <div className="flex flex-wrap justify-center gap-5 ">
       {filteredClips.map((clip) => (
         <DashboardCard
           key={clip.id}
-          Image={clip.thumbnail}
-          ClipperName={clip.user}
-          ClipperTitle={clip.name}
-          Platform={clip.platform}
-          Status={clip.status}
+          clip={clip}
+          openModal={setIsClipModalOpen}
         />
       ))}
     </div>
   );
 };
 
-export default SubmittedClips;
+export default DisplayClips;
