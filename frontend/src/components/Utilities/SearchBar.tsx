342be91627291e401dfee68f176e384080247c6d
JibAb96 <j.abshir96@gmail.com>
2025-01-19 15:31:55 +0100

    Add aria-labels to icons, inputs and buttons. Make remove animation for the SearchBar on smaller screen sizes.

--- frontend/src/components/Utilities/SearchBar.tsx
+++ frontend/src/components/Utilities/SearchBar.tsx
@@ -23,7 +23,7 @@ const SearchBar = ({
       className={`
         bg-white py-2.5 px-2 flex items-center w-2/3 rounded-full 
         border border-gray-300 shadow-md drop-shadow-2xl mx-auto mb-2 font-sans 
-        transition-all duration-300 
+        lg:transition-all lg:duration-300 
         ${isScrolled && !isSearchExpanded ? "lg:w-96" : "lg:max-w-2xl lg:w-1/2"}
       `} // Tailwind CSS class logic for responsive design, dynamic width and styling
     >
@@ -32,13 +32,15 @@ const SearchBar = ({
         onChange={(e) => setSearch(e.target.value)} // Update search state on text change
         placeholder="Search for clippers" // Placeholder text to guide the user
         className="w-full outline-none bg-transparent pl-4 text-base text-gray-700 placeholder-gray-400" // Tailwind styling for input field
+        aria-label="search-bar"   
       />
       <button
         type="button" // Button for submitting the search, although functionality is not implemented here
         className="ml-2 bg-secondary hover:bg-blue-600 text-white text-base rounded-full p-2 flex justify-center items-center" // Tailwind styling for button
+        aria-label="search button added for styling"
       >
         {/*Search icon from lucide-react with styling*/}
-        <Search className="text-white" size={20} />
+        <Search className="text-white" size={20} aria-hidden="false"/>
       </button>
     </div>
   );
