55d4d2c16852b26c7e7b20f233e7eda90e57b998
JibAb96 <j.abshir96@gmail.com>
2025-01-19 15:05:54 +0100

    Remove onClose prop to be used in modal content component. Make modal verticlly central on page and give some padding to ensure space from the edge of screens. Add a more aesthitically pleasing scrollbar. Add style for sizing and color of modal content.

--- frontend/src/components/Modals/Modal.tsx
+++ frontend/src/components/Modals/Modal.tsx
@@ -1,22 +1,33 @@
-import React, { MouseEventHandler, ReactNode } from "react";
+import React, { ReactNode } from "react";
 
 type Props = {
   isOpen: Boolean;
-  onClose: MouseEventHandler<HTMLButtonElement>;
   children: ReactNode;
 };
 
-const Modal = ({ isOpen, onClose, children }: Props) => {
+const Modal = ({ isOpen, children }: Props) => {
   if (!isOpen) return null; //Nothing to be returned if modal has not been signalled to be open
 
   return (
-    <div className="fixed inset-0 z-50 flex justify-center align-center mt-4">
+    <div className="fixed inset-0 z-50 flex justify-center items-center p-4">
       {/* Modal overlay */}
       <div
         className="fixed inset-1 bg-black bg-opacity-50"
         aria-hidden="true"
       />
-      {children}
+      <div
+        className="relative bg-primary rounded-lg max-w-[18rem] sm:max-w-lg p-6 
+    max-h-[90vh] overflow-y-auto
+    [&::-webkit-scrollbar]:w-2
+    [&::-webkit-scrollbar-track]:rounded-full
+    [&::-webkit-scrollbar-track]:bg-gray-100
+    [&::-webkit-scrollbar-thumb]:rounded-full
+    [&::-webkit-scrollbar-thumb]:bg-gray-300
+    dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+    dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
+      >
+        {children}
+      </div>
     </div>
   );
 };
