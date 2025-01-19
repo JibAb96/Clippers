3a6694873fd937175127d6b85f54c1e7c7715997
JibAb96 <j.abshir96@gmail.com>
2025-01-19 14:33:47 +0100

    Create a generic button component.

---
+++ frontend/src/components/Buttons/EmptyButton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type Props = {
+  CustomClass: string;
+  Text: string;
+  onClick?: () => void;
+  AriaLabel: string;
+};
+const EmptyButton = ({ CustomClass, Text, onClick, AriaLabel }: Props) => {
+  return (
+    // Reusable call to action button
+    <button
+      type="button"
+      onClick={onClick}
+      className={`${CustomClass} sm:block text-primary rounded-full font-medium text-sm px-3 h-12 text-center`}
+      aria-label={AriaLabel}
+    >
+      {Text}
+    </button>
+  );
+};
+
+export default EmptyButton;
