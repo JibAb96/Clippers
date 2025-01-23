937af8d55254d147e6fdc51e9c5e9c9aa16ea413
JibAb96 <j.abshir96@gmail.com>
2025-01-23 17:36:08 +0100

    Create a redux slice to set clips to be displayed on user dashboard and to also filter by status of clip.

---
+++ frontend/src/state/Clips/clips.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Clip } from "../../model";
+
+
+const initialState: Clip[] = [];
+
+const clips = createSlice({
+  name: "clips",
+  initialState,
+  reducers: {
+    filterClips: (state, action) => {
+      if (action.payload.status) {
+        let tempClips =action.payload.clips.filter(
+          (clip: Clip) => clip.status === action.payload.status
+        );
+        return tempClips.flat();
+      } else {
+       return action.payload.clips;
+      }
+    },
+  },
+});
+
+export const { filterClips } = clips.actions;
+export default clips.reducer;
