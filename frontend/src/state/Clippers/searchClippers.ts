b5235896ff1fa9b6b1aff20d963a296e3987b999
JibAb96 <j.abshir96@gmail.com>
2025-01-23 17:33:35 +0100

    Create redux slice to capture the input typed in the search bar by the user.

---
+++ frontend/src/state/Clippers/searchClippers.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type Search = {
+    value: string;
+}
+
+const initialState: Search = {
+    value: ""
+}
+
+const searchSlice = createSlice({
+    name: "search",
+    initialState,
+    reducers: {
+        setSearch: (state, action) => action.payload
+    }
+})
+
+export const { setSearch } = searchSlice.actions
+export default searchSlice.reducer
