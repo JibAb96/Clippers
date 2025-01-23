65b70e63e6676e3b17286673ea989f2f54639190
JibAb96 <j.abshir96@gmail.com>
2025-01-23 12:18:31 +0100

    Add the selectCategory and filter Clippers redux slices with their reducers to the redux store

--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./SearchClippers/searchClippers";
+import selectCategoryReducer from "./FilterClippers/selectCategory"
+import filterClippersReducer from "./FilterClippers/filterClippers"
 export const store = configureStore({
   reducer: {
     search: searchReducer,
+    selectCategory: selectCategoryReducer,
+    clippers: filterClippersReducer
   },
 });
 
