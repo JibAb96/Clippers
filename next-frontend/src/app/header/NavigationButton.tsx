5abbee8b92f71ccf35a20c7897dc5091a5d99e04
JibAb96 <j.abshir96@gmail.com>
2025-02-13 13:53:39 +0100

    Add closeDropdown function to onClick property for Home nav link.

--- next-frontend/src/app/header/NavigationButton.tsx
+++ next-frontend/src/app/header/NavigationButton.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import { HiMenu, HiUser } from "react-icons/hi";
 import Link from "next/link";
@@ -42,6 +42,7 @@ const NavigationButton = () => {
           >
             <li>
               <Link
+                onClick={closeDropdown}
                 href="/"
                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
               >
