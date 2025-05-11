28738532551fe777b2d3a26a2140471fd42bab54
JibAb96 <j.abshir96@gmail.com>
2025-05-11 17:33:09 +0200

    Update API base URL to production environment, enhancing API connectivity for the application.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001";
+const API_BASE_URL = "https://clippers-backend.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  // headers: {
-  //   "Content-Type": "application/json",
-  // },
 });
 
 // Request interceptor for authentication
