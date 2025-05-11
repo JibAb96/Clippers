72df9b0d9dd5a1b3fa11f4ab15034d7f26c30f56
JibAb96 <j.abshir96@gmail.com>
2025-05-11 16:29:12 +0200

    Add Reviews component to display creator reviews in Clipper profiles, enhancing user engagement and feedback visibility.

---
+++ src/app/clipper/[id]/components/Reviews.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Reviews = () => {
+  //Section of Clipper profiles displaying reviews from other creators.
+  return (
+    <div className=" ml-5 my-3 lg:mx-20 xl:my-10">
+      <h2 className="text-secondary font-semibold text-xl sm:text-2xl  xl:text-3xl">Creator Reviews</h2>
+      <div>
+       Reviews
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
