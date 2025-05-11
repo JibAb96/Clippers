947b2b18d1240bedeb536f0958f96421504895f1
JibAb96 <118972033+JibAb96@users.noreply.github.com>
2025-05-11 16:45:38 +0200

    Delete next-frontend directory

--- next-frontend/src/app/profile/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import UserProfile from "./components/UserProfile";
-
-const Page = () => {
-  return <UserProfile />;
-};
-
-export default Page;
--- next-frontend/src/app/register/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Register from "./component/Register";
-
-const Page = () => {
-  return <Register />;
-};
-
-export default Page;
