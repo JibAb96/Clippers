f446019a0a7d9cb6a8c484f9e73f7bbb586ca1e7
JibAb96 <j.abshir96@gmail.com>
2025-05-11 16:32:27 +0200

    Add Page component for CreatorDashboard, establishing the main entry point for the creator dashboard interface and rendering the CreatorDashboard component for improved user interaction.

---
+++ src/app/dashboard/creator/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import React from "react";
+import CreatorDashboard from "../components/CreatorDashboard";
+
+const Page = () => {
+  return <CreatorDashboard />;
+};
+
+export default Page;
