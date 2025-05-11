885fddd9c5d5f7550872a74ed022b981364b7526
JibAb96 <j.abshir96@gmail.com>
2025-05-11 16:28:55 +0200

    Add Guidlines component to display submission and custom guidelines for video clips, enhancing clarity and compliance for users during the submission process.

---
+++ src/app/clipper/[id]/components/Guidlines.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Guidlines = ({ guidelines }: { guidelines: string[] }) => {
+  //Displaying guideline information for clippers
+  return (
+    <div className="ml-5 my-3 flex flex-col gap-5 md:flex-row xl:text-lg lg:mx-20 xl:my-10">
+      <div>
+        <h2 className="text-secondary font-semibold text-xl sm:text-2xl  xl:text-3xl">
+          Submission Guidlines
+        </h2>
+        <div className="xl:mt-5 font-medium flex flex-col gap-2 xl:gap-4 ">
+          <p>
+            <span className="font-bold">Video Length:</span> Maximum 90 seconds
+          </p>
+          <p>
+            <span className="font-bold">Video Format:</span> MP4
+          </p>
+          <p>
+            <span className="font-bold">Video Resolution:</span> Minimum 1080p
+          </p>
+          <p>
+            All submissions <span className="font-bold">must</span> comply with
+            copyright laws
+          </p>
+          <p>
+            All submissions <span className="font-bold">must</span> comply with
+            community guidlines
+          </p>
+          <p>
+            Submissions only posted <span className="font-bold">after</span>{" "}
+            full payment confirmation
+          </p>
+          <p>Clips will be posted at agreed scheduled date and time</p>
+        </div>
+      </div>
+      <div>
+        <h2 className="text-secondary text-xl font-semibold sm:text-2xl xl:text-3xl">
+          Custom Guidlines
+        </h2>
+        <div className="xl:mt-5 font-medium flex flex-col xl:gap-4">
+          {guidelines.map((guideline, index) => (
+            <p key={index}>{guideline}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Guidlines;
