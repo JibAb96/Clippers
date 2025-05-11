8f356eb13e6424ec4f4ad0b04ddaa78942c1d6a7
JibAb96 <j.abshir96@gmail.com>
2025-05-11 16:37:06 +0200

    Add Modal component for displaying content in a centered overlay, enhancing user experience with customizable children and visibility control.

---
+++ src/components/Modal.tsx
@@ -0,0 +1,37 @@
+"use client"
+import React, { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  isOpen: boolean;
+};
+
+const Modal = ({  children, isOpen }: Props) => {
+
+  if (!isOpen) return null; //Nothing to be returned if modal has not been signalled to be open
+
+  return (
+    <div className="fixed inset-0 z-50 flex justify-center items-center p-4">
+      {/* Modal overlay */}
+      <div
+        className="fixed inset-1 bg-black bg-opacity-50"
+        aria-hidden="true"
+      />
+      <div
+        className="relative bg-primary rounded-lg max-w-2xs sm:max-w-3xl p-6 
+    max-h-[90vh] overflow-y-auto
+    [&::-webkit-scrollbar]:w-2
+    [&::-webkit-scrollbar-track]:rounded-full
+    [&::-webkit-scrollbar-track]:bg-gray-100
+    [&::-webkit-scrollbar-thumb]:rounded-full
+    [&::-webkit-scrollbar-thumb]:bg-gray-300
+    dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+    dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
