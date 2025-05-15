276a54c909dfd06edcccd0bede2c7804124ba2de
JibAb96 <j.abshir96@gmail.com>
2025-05-15 11:45:43 +0200

    Update API base URL to use environment variable for improved flexibility in different environments.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://clippers-backend.onrender.com";
-
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
