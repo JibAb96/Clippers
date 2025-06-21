56532069ece397456636c65df773f9e41fd7321d
JibAb96 <j.abshir96@gmail.com>
2025-06-21 15:33:55 +0200

    fix: correct component import name from Login to SignIn in signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 import React from "react"
-import Login from "./components/SignIn"
+import SignIn from "./components/SignIn"
 
 const Page = () => {
   return (
-    <Login/>
+    <SignIn/>
   )
 }
 
