d7374795bfe787354da9b1d7dcddaea386f955fa
JibAb96 <j.abshir96@gmail.com>
2025-06-21 15:34:19 +0200

    feat: add forgot password functionality and integrate ForgotPasswordModal in login form

--- src/app/signin/components/Form.tsx
+++ src/app/signin/components/Form.tsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 import { loginCreator, loginClipper } from "../../../state/User/userThunks";
 import { RootState, AppDispatch } from "../../../state/store";
+import { setForgotPassword } from "../../../state/Modal/isOpen";
+import ForgotPasswordModal from "./ForgotPasswordModal";
 
 const Form = () => {
   const [formData, setFormData] = useState({
@@ -39,124 +41,125 @@ const Form = () => {
     }
   };
 
+  const handleForgotPassword = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    dispatch(setForgotPassword());
+  };
+
   useEffect(() => {
-    console.log(
-      "[Login Form useEffect] Running effect - User:",
-      user,
-      "Loading:",
-      loading,
-      "Error:",
-      error,
-      "UserType:",
-      loggedInUserType
-    );
     if (user && !loading && !error && loggedInUserType) {
-      console.log(
-        "[Login Form useEffect] Conditions met for navigation. User data:",
-        user,
-        "Role:",
-        loggedInUserType
-      );
       if (loggedInUserType === "creator") {
-        router.push("/dashboard/creator"); // Or your actual creator dashboard path
+        router.push("/dashboard/creator");
       } else if (loggedInUserType === "clipper") {
-        router.push("/dashboard/clipper"); // Or your actual clipper dashboard path
+        router.push("/dashboard/clipper"); 
       } else {
-        router.push("/"); // Fallback, though userType should be set
+        router.push("/");
       }
     }
   }, [user, loading, error, loggedInUserType, router]);
 
   return (
-    <form className="space-y-6" onSubmit={handleSubmit}>
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-700 mb-2">
-          Log in as:
-        </label>
-        <div className="flex rounded-md shadow-sm">
-          <button
-            type="button"
-            onClick={() => setUserType("creator")}
-            className={`px-4 py-2 border border-gray-300 text-sm font-medium 
-                            ${
-                              userType === "creator"
-                                ? "bg-gray-800 text-white z-10 ring-2 ring-gray-700"
-                                : "bg-white text-gray-700 hover:bg-gray-50"
-                            }
-                            rounded-l-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors duration-150 ease-in-out`}
+    <>
+      <form className="space-y-6" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Log in as:
+          </label>
+          <div className="flex rounded-md shadow-sm">
+            <button
+              type="button"
+              onClick={() => setUserType("creator")}
+              className={`px-4 py-2 border border-gray-300 text-sm font-medium 
+                              ${
+                                userType === "creator"
+                                  ? "bg-gray-800 text-white z-10 ring-2 ring-gray-700"
+                                  : "bg-white text-gray-700 hover:bg-gray-50"
+                              }
+                              rounded-l-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors duration-150 ease-in-out`}
+            >
+              Creator
+            </button>
+            <button
+              type="button"
+              onClick={() => setUserType("clipper")}
+              className={`-ml-px px-4 py-2 border border-gray-300 text-sm font-medium 
+                              ${
+                                userType === "clipper"
+                                  ? "bg-gray-800 text-white z-10 ring-2 ring-gray-700"
+                                  : "bg-white text-gray-700 hover:bg-gray-50"
+                              }
+                              rounded-r-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors duration-150 ease-in-out`}
+            >
+              Clipper
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <label className="font-bold" htmlFor="email">
+            Email
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            className="bg-gray-100 focus:bg-transparent w-full text-sm px-4 py-3.5 rounded-md outline-gray-800"
+            placeholder="Email address"
+          />
+        </div>
+        <div>
+          <label className="font-bold" htmlFor="password">
+            Password
+          </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="current-password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+            className="bg-gray-100 focus:bg-transparent w-full text-sm px-4 py-3.5 rounded-md outline-gray-800"
+            placeholder="Password"
+          />
+        </div>
+        <div className="text-sm text-right">
+          <a
+            href="#"
+            onClick={handleForgotPassword}
+            className="text-secondary font-semibold hover:underline"
           >
-            Creator
-          </button>
+            Forgot your password?
+          </a>
+        </div>
+
+        {error && (
+          <div className="text-red-500 text-sm bg-red-100 border border-red-400 p-3 rounded-md">
+            {error ===
+            "There was an internal server error during login process."
+              ? "Invalid email or password."
+              : error}
+          </div>
+        )}
+
+        <div>
           <button
-            type="button"
-            onClick={() => setUserType("clipper")}
-            className={`-ml-px px-4 py-2 border border-gray-300 text-sm font-medium 
-                            ${
-                              userType === "clipper"
-                                ? "bg-gray-800 text-white z-10 ring-2 ring-gray-700"
-                                : "bg-white text-gray-700 hover:bg-gray-50"
-                            }
-                            rounded-r-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors duration-150 ease-in-out`}
+            type="submit"
+            disabled={loading}
+            className="w-full shadow-xl py-3 px-6 text-sm font-semibold rounded-md text-white bg-gray-800 hover:bg-[#222] focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
           >
-            Clipper
+            {loading ? "Logging in..." : "Log in"}
           </button>
         </div>
-      </div>
+      </form>
 
-      <div>
-        <label className="font-bold" htmlFor="email">
-          Email
-        </label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          autoComplete="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-          className="bg-gray-100 focus:bg-transparent w-full text-sm px-4 py-3.5 rounded-md outline-gray-800"
-          placeholder="Email address"
-        />
-      </div>
-      <div>
-        <label className="font-bold" htmlFor="password">
-          Password
-        </label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          autoComplete="current-password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-          className="bg-gray-100 focus:bg-transparent w-full text-sm px-4 py-3.5 rounded-md outline-gray-800"
-          placeholder="Password"
-        />
-      </div>
-      <div className="text-sm text-right">
-        <a href="#" className="text-secondary font-semibold hover:underline">
-          Forgot your password?
-        </a>
-      </div>
-
-      {error && (
-        <div className="text-red-500 text-sm bg-red-100 border border-red-400 p-3 rounded-md">
-          {error}
-        </div>
-      )}
-
-      <div>
-        <button
-          type="submit"
-          disabled={loading}
-          className="w-full shadow-xl py-3 px-6 text-sm font-semibold rounded-md text-white bg-gray-800 hover:bg-[#222] focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
-        >
-          {loading ? "Logging in..." : "Log in"}
-        </button>
-      </div>
-    </form>
+      {/* Forgot Password Modal */}
+      <ForgotPasswordModal />
+    </>
   );
 };
 
