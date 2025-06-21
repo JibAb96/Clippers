d09378495d6721f0a82a837740edc10e7920fd0e
JibAb96 <j.abshir96@gmail.com>
2025-06-21 15:42:58 +0200

    feat: add forgotPassword state to Modal slice and update initial state

--- src/state/Modal/isOpen.tsx
+++ src/state/Modal/isOpen.tsx
@@ -1,18 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
-type Modals = {clipSubmission: boolean, submittedClips: boolean, logout: boolean, profileForm: boolean }
-const initialState: Modals  = {clipSubmission: false, submittedClips: false, logout: false, profileForm: false};
+type Modals = {
+  clipSubmission: boolean;
+  submittedClips: boolean;
+  logout: boolean;
+  profileForm: boolean;
+  forgotPassword: boolean;
+};
+const initialState: Modals = {
+  clipSubmission: false,
+  submittedClips: false,
+  logout: false,
+  profileForm: false,
+  forgotPassword: false,
+};
 
 const isOpen = createSlice({
-    name:"isOpen",
-    initialState,
-    reducers: {
-        setClipSubmission: (state) => {state.clipSubmission = !state.clipSubmission},
-        setSubmittedClip: (state) => {state.submittedClips = !state.submittedClips},
-        setLogout: (state) => {state.logout = !state.logout},
-        setProfileForm: (state) => {state.profileForm = !state.profileForm}
-    }
-})
+  name: "isOpen",
+  initialState,
+  reducers: {
+    setClipSubmission: (state) => {
+      state.clipSubmission = !state.clipSubmission;
+    },
+    setSubmittedClip: (state) => {
+      state.submittedClips = !state.submittedClips;
+    },
+    setLogout: (state) => {
+      state.logout = !state.logout;
+    },
+    setProfileForm: (state) => {
+      state.profileForm = !state.profileForm;
+    },
+    setForgotPassword: (state) => {
+      state.forgotPassword = !state.forgotPassword;
+    },
+  },
+});
 
-export const { setClipSubmission, setSubmittedClip, setLogout, setProfileForm } = isOpen.actions
+export const {
+  setClipSubmission,
+  setSubmittedClip,
+  setLogout,
+  setProfileForm,
+  setForgotPassword,
+} = isOpen.actions;
 
-export default isOpen.reducer
+export default isOpen.reducer;
