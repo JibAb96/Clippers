582042026529a88cdb79a0f7f6376fd07e44ee2b
JibAb96 <j.abshir96@gmail.com>
2025-06-21 15:34:38 +0200

    fix: rename component from Login to SignIn and update related text for clarity

--- src/app/signin/components/SignIn.tsx
+++ src/app/signin/components/SignIn.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import Background from "./Background";
 import Form from "./Form";
 
-
-const Login = () => {
+const SignIn = () => {
   //Sign page for users
 
   return (
@@ -12,20 +11,19 @@ const Login = () => {
         <div className="grid md:grid-cols-2 items-center gap-10 max-w-6xl w-full">
           <div className="p-4">
             <h1 className="text-4xl  font-extrabold lg:leading-[50px] ">
-              <span className="text-secondary">Seamless Login</span> for
-              Exclusive Access
+              <span className="text-secondary">Access Your Content Hub</span> -
+              Distribute Clips
             </h1>
             <p className="text-sm mt-6 ">
-              Immerse yourself in a hassle-free login journey with our
-              intuitively designed login form. Effortlessly access your account.
+              Join thousands of creators and clippers in the premier
+              marketplace for video clips. Sign in to discover premium content,
+              manage your portfolio, and grow your audience.
             </p>
           </div>
 
-          <div
-            className="bg-white sm:shadow-lg sm:rounded-xl px-6 py-8 max-w-md mx-auto md:ml-auto w-full"
-          >
-            <h2 className="text-3xl font-extrabold mb-12">Sign in</h2>
-            <Form/>
+          <div className="bg-white sm:shadow-lg sm:rounded-xl px-6 py-8 max-w-md mx-auto md:ml-auto w-full">
+            <h2 className="text-3xl font-extrabold mb-12">Welcome Back</h2>
+            <Form />
           </div>
         </div>
       </div>
@@ -33,4 +31,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignIn;
