c152a52ba4d2852d955f9806ef0a36592401fd59
JibAb96 <j.abshir96@gmail.com>
2025-07-03 14:30:27 +0200

    refactor: update CTAButton component to use children prop and include aria-label

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -1,27 +1,39 @@
-"use client"
+"use client";
 import React from "react";
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 
 type Props = {
   CustomClass?: string;
-  Text: string;
-  onClick?: string | (() => void);  
-  AriaLabel: string;
+  children: React.ReactNode;
+  onClick?: string | (() => void);
+  "aria-label"?: string;
   type?: "button" | "submit" | "reset" | undefined;
-  disabled?: boolean
+  AriaLabel?: string;
+  disabled?: boolean;
 };
-const CTAButton = ({ CustomClass, onClick, Text, AriaLabel, type = "button", disabled }: Props) => {
-const router = useRouter();
+const CTAButton = ({
+  CustomClass,
+  onClick,
+  children,
+  type = "button",
+  disabled,
+  AriaLabel,
+  ...props
+}: Props) => {
+  const router = useRouter();
   return (
     // Reusable call to action button
     <button
       type={type}
-      onClick={typeof onClick === "string" ? () => router.push(onClick) : onClick}
+      onClick={
+        typeof onClick === "string" ? () => router.push(onClick) : onClick
+      }
       className={`${CustomClass} sm:block text-white rounded-full bg-gradient-to-r from-blue-700 to-secondary font-medium text-sm px-3 h-12 text-center`}
-      aria-label={AriaLabel}
       disabled={disabled}
+    aria-label={AriaLabel}
+      {...props}
     >
-      {Text}
+      {children}
     </button>
   );
 };
