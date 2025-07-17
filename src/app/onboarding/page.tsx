9318b8a526b6b78fa478988e854593f9b6945c7e
JibAb96 <j.abshir96@gmail.com>
2025-07-17 12:49:29 +0200

    feat: add OnboardingPage component to handle onboarding flow

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import OnboardingFlow from "./components/OnboardingFlow";
+
+const OnboardingPage = () => {
+  return <OnboardingFlow />;
+};
+
+export default OnboardingPage;
