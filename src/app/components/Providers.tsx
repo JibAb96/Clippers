a2a74a9173ddcec9fd27832781ef37238075923f
JibAb96 <j.abshir96@gmail.com>
2025-07-20 11:28:57 +0200

    feat: integrate TutorialProvider and TutorialWrapper into Providers component for enhanced tutorial functionality

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -2,12 +2,18 @@
 
 import StoreProvider from "@/state/StoreProvider";
 import ClientSessionProvider from "@/app/components/ClientSessionProvider";
+import { TutorialProvider } from "../../contexts/TutorialContext";
+import TutorialWrapper from "./TutorialWrapper";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <StoreProvider>
       <ClientSessionProvider/>
+      <TutorialProvider>
+        <TutorialWrapper>
         {children}
+        </TutorialWrapper>
+      </TutorialProvider>
     </StoreProvider>
   );
 }
